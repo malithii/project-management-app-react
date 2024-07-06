d3d054d6001a78eb79b5292fce12538532075888
Malithi Karunarathne <mkarunarathne997@gmail.com>
2024-07-06 10:21:37 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
+function App() {
+
+  const[projectsState,setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:null
+      }
+  });
+  }
+
+  function handleCandelAddProject(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:undefined
+      }
+  });
+  }
+
+  function handleAddProject(projectData){
+    setProjectsState(prevState=>{
+      const projectId=Math.random();
+      const newProject={
+        ...projectData,
+        id:Math.random()
+      };
+      return{
+        ...prevState,
+        selectedProjectId:undefined,
+        projects:[...prevState.projects,newProject],
+
+      }
+    })
+  }
+
+ 
+  let content;
+
+  if(projectsState.selectedProjectId===null){
+      content = <NewProject onAdd={handleAddProject} onCancel={handleCandelAddProject}/>
+
+  }else if(projectsState.selectedProjectId===undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
+    </main>
+  );
+}
+
+export default App;
